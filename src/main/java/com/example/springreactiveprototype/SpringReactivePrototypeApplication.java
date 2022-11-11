afd6ebd30b7e01d8c0dee48d458ca3e26c37c1a2
Kareem Mohamed <kmohamed.dev@gmail.com>
2022-11-11 15:19:16 +0200

    init project files

---
+++ src/main/java/com/example/springreactiveprototype/SpringReactivePrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springreactiveprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactivePrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactivePrototypeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springreactiveprototype/SpringReactivePrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springreactiveprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactivePrototypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
