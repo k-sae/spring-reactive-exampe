9a0131c70b9651d13e8b2066abd5306af7025624
Kareem Mohamed <kmohamed.dev@gmail.com>
2022-11-12 13:52:23 +0200

    add authorization support from unit tests

--- src/test/java/com/example/springreactiveprototype/domain/TestUserController.java
+++ src/test/java/com/example/springreactiveprototype/domain/TestUserController.java
@@ -2,6 +2,9 @@ package com.example.springreactiveprototype.domain;
 import com.example.springreactiveprototype.domain.user.model.User;
 import com.example.springreactiveprototype.domain.user.payload.SaveUserRequestDTO;
 import com.example.springreactiveprototype.domain.user.service.UserService;
+import com.example.springreactiveprototype.util.JWTUtil;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +28,16 @@ public class TestUserController {
     @MockBean
     private UserService service;
 
+    private String token;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @BeforeEach
+    public void setup(){
+        token = "Bearer " + jwtUtil.generateToken(User.builder().name("testUser").build());
+    }
+
     @Test
     public void addUserTest(){
         SaveUserRequestDTO saveUserRequestDTO = new SaveUserRequestDTO(
@@ -33,7 +46,8 @@ public class TestUserController {
                 "test father",
                 1
         );
-        webTestClient.post().uri("/users")
+        webTestClient.post().uri("/students")
+                .header("Authorization", token)
                 .body(Mono.just(saveUserRequestDTO), User.class)
                 .exchange()
                 .expectStatus().isCreated();
@@ -50,7 +64,8 @@ public class TestUserController {
 
         when(service.getUser(any())).thenReturn(Mono.just(expected));
 
-        Flux<User> responseBody = webTestClient.get().uri("/users/testId")
+        Flux<User> responseBody = webTestClient.get().uri("/students/testId")
+                .header("Authorization", token)
                 .exchange()
                 .expectStatus().isOk()
                 .returnResult(User.class)
